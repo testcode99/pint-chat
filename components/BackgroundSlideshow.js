0d78afa4b4dd473c52b8b027d38726b6af9997d9
testcode99 <shnbreen@gmail.com>
2025-08-20 21:40:57 +0100

    changed background image timer

--- components/BackgroundSlideshow.js
+++ components/BackgroundSlideshow.js
@@ -21,7 +21,7 @@ const BackgroundSlideshow = ({ images }) => {
         setCurrentImageIndex((prevIndex) => (prevIndex + 1) % shuffledImages.length);
         setIsFading(false);
       }, 3); // Fade duration
-    }, 750); // Time each image is visible
+    }, 950); // Time each image is visible
 
     return () => clearInterval(interval);
   }, [shuffledImages]);
